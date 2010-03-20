d521bcaf6b5921cb0418a5cd2cc2d4438ec47feb
void <git.email.address@googlemail.com>
2010-03-20 12:36:01 +0100

    Fixed a bug, fixing the formatting

--- mounter.cpp
+++ mounter.cpp
@@ -1,158 +1,134 @@
 #define _WIN32_DCOM
 #include <iostream>
-using namespace std;
 #include <comdef.h>
 #include <Wbemidl.h>
 
-# pragma comment(lib, "wbemuuid.lib")
-
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
-	HRESULT hres;
-
-	// Step 1: --------------------------------------------------
-	// Initialize COM. ------------------------------------------
-
-	hres =  CoInitializeEx(0, COINIT_MULTITHREADED); 
-	if (FAILED(hres))
+	HRESULT result = CoInitializeEx(0, COINIT_MULTITHREADED); 
+	if(FAILED(result))
 	{
-		cout << "Failed to initialize COM library. Error code = 0x" 
-			<< hex << hres << endl;
-		return 1;				  // Program has failed.
+		std::cout << "Failed to initialize COM library. Error code = 0x" << std::hex << result << std::endl;
+		return 1;
 	}
 
-	// Step 2: --------------------------------------------------
-	// Set general COM security levels --------------------------
-	// Note: If you are using Windows 2000, you need to specify -
-	// the default authentication credentials for a user by using
-	// a SOLE_AUTHENTICATION_LIST structure in the pAuthList ----
-	// parameter of CoInitializeSecurity ------------------------
+	result = CoInitializeSecurity
+	(
+		0, 
+		-1, //COM authentication
+		0, //Authentication services
+		0, //Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT, //Default authentication 
+		RPC_C_IMP_LEVEL_IMPERSONATE, //Default Impersonation 
+		0, //Authentication info
+		EOAC_NONE, //Additional capabilities 
+		0 //Reserved
+	);
 
-	hres =  CoInitializeSecurity(
-		NULL, 
-		-1,						  // COM authentication
-		NULL,						// Authentication services
-		NULL,						// Reserved
-		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
-		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
-		NULL,						// Authentication info
-		EOAC_NONE,				   // Additional capabilities 
-		NULL						 // Reserved
-		);
-
-					  
-	if (FAILED(hres))
+					 
+	if(FAILED(result))
 	{
-		cout << "Failed to initialize security. Error code = 0x" 
-			<< hex << hres << endl;
+		std::cout << "Failed to initialize security. Error code = 0x" << std::hex << result << std::endl;
 		CoUninitialize();
-		return 1;					// Program has failed.
+		return 1;
 	}
-	
-	// Step 3: ---------------------------------------------------
-	// Obtain the initial locator to WMI -------------------------
 
-	IWbemLocator *pLoc = NULL;
+	IWbemLocator * locator = 0;
 
-	hres = CoCreateInstance(
+	result = CoCreateInstance
+	(
 		CLSID_WbemLocator,			 
 		0, 
 		CLSCTX_INPROC_SERVER, 
-		IID_IWbemLocator, (LPVOID *) &pLoc);
+		IID_IWbemLocator,
+		reinterpret_cast<LPVOID *>(&locator)
+	);
  
-	if (FAILED(hres))
+	if(FAILED(result))
 	{
-		cout << "Failed to create IWbemLocator object."
-			<< " Err code = 0x"
-			<< hex << hres << endl;
+		std::cout << "Failed to create IWbemLocator object. Err code = 0x" << std::hex << result << std::endl;
 		CoUninitialize();
-		return 1;				 // Program has failed.
+		return 1;
 	}
 
-	// Step 4: -----------------------------------------------------
-	// Connect to WMI through the IWbemLocator::ConnectServer method
-
-	IWbemServices *pSvc = NULL;
+	IWbemServices * pSvc = 0;
 	
-	// Connect to the root\cimv2 namespace with
-	// the current user and obtain pointer pSvc
-	// to make IWbemServices calls.
-	hres = pLoc->ConnectServer(
-		 _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
-		 NULL,					// User name. NULL = current user
-		 NULL,					// User password. NULL = current
-		 0,					   // Locale. NULL indicates current
-		 NULL,					// Security flags.
-		 0,					   // Authority (e.g. Kerberos)
-		 0,					   // Context object 
-		 &pSvc					// pointer to IWbemServices proxy
+	result = locator->ConnectServer(
+		 _bstr_t(L"ROOT\\CIMV2"), //Object path of WMI namespace
+		 0,					//User name. 0 = current user
+		 0,					//User password. 0 = current
+		 0,					 //Locale. 0 indicates current
+		 0,					//Security flags.
+		 0,					 //Authority (e.g. Kerberos)
+		 0,					 //Context object 
+		 &pSvc					//pointer to IWbemServices proxy
 		 );
 	
-	if (FAILED(hres))
+	if(FAILED(result))
 	{
-		cout << "Could not connect. Error code = 0x" 
-			 << hex << hres << endl;
-		pLoc->Release();	 
+		std::cout << "Could not connect. Error code = 0x" 
+			 << std::hex << result << std::endl;
+		locator->Release();	 
 		CoUninitialize();
-		return 1;				// Program has failed.
+		return 1;				//Program has failed.
 	}
 
-	cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
+	std::cout << "Connected to ROOT\\CIMV2 WMI namespace" << std::endl;
 
 
-	// Step 5: --------------------------------------------------
-	// Set security levels on the proxy -------------------------
+	//Step 5: --------------------------------------------------
+	//Set security levels on the proxy -------------------------
 
-	hres = CoSetProxyBlanket(
-	   pSvc,						// Indicates the proxy to set
-	   RPC_C_AUTHN_WINNT,		   // RPC_C_AUTHN_xxx
-	   RPC_C_AUTHZ_NONE,			// RPC_C_AUTHZ_xxx
-	   NULL,						// Server principal name 
-	   RPC_C_AUTHN_LEVEL_CALL,	  // RPC_C_AUTHN_LEVEL_xxx 
-	   RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-	   NULL,						// client identity
-	   EOAC_NONE					// proxy capabilities 
+	result = CoSetProxyBlanket(
+	 pSvc,						//Indicates the proxy to set
+	 RPC_C_AUTHN_WINNT,		 //RPC_C_AUTHN_xxx
+	 RPC_C_AUTHZ_NONE,			//RPC_C_AUTHZ_xxx
+	 0,						//Server principal name 
+	 RPC_C_AUTHN_LEVEL_CALL,	 //RPC_C_AUTHN_LEVEL_xxx 
+	 RPC_C_IMP_LEVEL_IMPERSONATE, //RPC_C_IMP_LEVEL_xxx
+	 0,						//client identity
+	 EOAC_NONE					//proxy capabilities 
 	);
 
-	if (FAILED(hres))
+	if(FAILED(result))
 	{
-		cout << "Could not set proxy blanket. Error code = 0x" 
-			<< hex << hres << endl;
+		std::cout << "Could not set proxy blanket. Error code = 0x" 
+			<< std::hex << result << std::endl;
 		pSvc->Release();
-		pLoc->Release();	 
+		locator->Release();	 
 		CoUninitialize();
-		return 1;			   // Program has failed.
+		return 1;			 //Program has failed.
 	}
 
-	// Step 6: --------------------------------------------------
-	// Use the IWbemServices pointer to make requests of WMI ----
+	//Step 6: --------------------------------------------------
+	//Use the IWbemServices pointer to make requests of WMI ----
 
-	// For example, get the name of the operating system
-	IEnumWbemClassObject* pEnumerator = NULL;
-	hres = pSvc->ExecQuery(
+	//For example, get the name of the operating system
+	IEnumWbemClassObject* pEnumerator = 0;
+	result = pSvc->ExecQuery(
 		bstr_t("WQL"), 
 		bstr_t("SELECT * FROM Win32_OperatingSystem"),
 		WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, 
-		NULL,
+		0,
 		&pEnumerator);
 	
-	if (FAILED(hres))
+	if(FAILED(result))
 	{
-		cout << "Query for operating system name failed."
+		std::cout << "Query for operating system name failed."
 			<< " Error code = 0x" 
-			<< hex << hres << endl;
+			<< std::hex << result << std::endl;
 		pSvc->Release();
-		pLoc->Release();
+		locator->Release();
 		CoUninitialize();
-		return 1;			   // Program has failed.
+		return 1;			 //Program has failed.
 	}
 
-	// Step 7: -------------------------------------------------
-	// Get the data from the query in step 6 -------------------
+	//Step 7: -------------------------------------------------
+	//Get the data from the query in step 6 -------------------
  
 	IWbemClassObject *pclsObj;
 	ULONG uReturn = 0;
-   
+ 
 	while (pEnumerator)
 	{
 		HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, 
@@ -165,23 +141,18 @@ int main(int argc, char **argv)
 
 		VARIANT vtProp;
 
-		// Get the value of the Name property
+		//Get the value of the Name property
 		hr = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
-		wcout << " OS Name : " << vtProp.bstrVal << endl;
+		std::wcout << " OS Name : " << vtProp.bstrVal << std::endl;
 		VariantClear(&vtProp);
 
 		pclsObj->Release();
 	}
 
-	// Cleanup
-	// ========
-	
 	pSvc->Release();
-	pLoc->Release();
+	locator->Release();
 	pEnumerator->Release();
-	pclsObj->Release();
 	CoUninitialize();
 
-	return 0;   // Program successfully completed.
-	
+	return 0;	
 }
